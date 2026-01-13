62bf33446cac4f5b2c1f39abf4fdcfc3d9a170a9
fs0711 <cs_1014@hotmail.com>
2026-01-13 22:46:27 +0500

    increase worker size

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,7 @@ import os
 os.environ["APP_ENVIRONMENT"] = "STAGING"
 
 bind = "unix:esp32ota.sock"
-workers = 1
+workers = 5
 accesslog = "/var/log/esp32ota/access.log"
 errorlog = "/var/log/esp32ota/error.log"
 umask = 7
